68a265ac2dfe89cb7372e9f4274651e38a7b4087
Guo Ji Xuan <S10222055@connect.np.edu.sg>
2022-12-13 21:00:13 +0800

    Timer UI in progress

--- Base URP with Firebase XRTK/Base URP with Firebase XRTK/Assets/Scripts/AICustomer1.cs
+++ Base URP with Firebase XRTK/Base URP with Firebase XRTK/Assets/Scripts/AICustomer1.cs
@@ -2,18 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class AICustomer1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI timerText;
+    public Image timerBar;
+
+    float waitTime;
+    float currentTime;
+
     void Start()
     {
-        
+        waitTime = Random.Range(10, 20);
+        currentTime = waitTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentTime -= 1 * Time.deltaTime;
+        timerText.text = currentTime.ToString("0") + "s";
+        TimerBarFiller();
+    }
+
+    void TimerBarFiller()
+    {
+        timerBar.fillAmount = currentTime / waitTime;   
     }
 }
