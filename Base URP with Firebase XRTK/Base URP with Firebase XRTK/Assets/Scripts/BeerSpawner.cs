dfe7195c2ca47a2f9f81747d101cd0d2ae1b44a4
Guo Ji Xuan <S10222055@connect.np.edu.sg>
2022-12-15 02:01:02 +0800

    Progress with Beer spawning

---
+++ Base URP with Firebase XRTK/Base URP with Firebase XRTK/Assets/Scripts/BeerSpawner.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeerSpawner : MonoBehaviour
+{
+    public GameObject beer;
+    // Spawn spots array
+    public GameObject[] spawnSpots;
+    // List stores currently used spots
+    public List<string> usedSpots = new List<string>();
+    public bool gameIsActive;
+    public bool spotsFull;
+
+    public bool isCoroutineStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i <= spawnSpots.Length; i++)
+        {
+            // Instantiate the Beer at the spawn spot and orient to face the right direction
+            Instantiate(beer, spawnSpots[i].transform.position, spawnSpots[i].transform.rotation);
+            string j = i.ToString();
+            usedSpots.Add(j);
+        }
+
+        StartCoroutine("SpawnBeer");
+    }
+
+    IEnumerator SpawnBeer()
+    {
+        isCoroutineStarted = true;
+        while (gameIsActive && !spotsFull)
+        {
+            yield return new WaitForSeconds(4f);
+            // Randomise spot to spawn at
+            int randomSpot = Random.Range(0, spawnSpots.Length);
+            string spotToAdd = randomSpot.ToString();
+
+            for (int i = 0; i <= spawnSpots.Length; i++)
+            {
+                string j = i.ToString();
+                if (!usedSpots.Contains(j) && spotToAdd == j)
+                {
+                    // Add the used spot to the usedSpots list
+                    usedSpots.Add(spotToAdd);
+                    // Instantiate the beer at the spot
+                    Instantiate(beer, spawnSpots[randomSpot].transform.position, spawnSpots[randomSpot].transform.rotation);
+                }
+            }
+        }
+        isCoroutineStarted = false;
+        yield return null;
+    }
+
+    private void Update()
+    {
+        if (usedSpots.Count == spawnSpots.Length)
+        {
+            spotsFull = true;
+        }
+
+        if (usedSpots.Count < spawnSpots.Length)
+        {
+            spotsFull = false;
+            if (!isCoroutineStarted)
+            {
+                StartCoroutine("SpawnBeer");
+            }
+        }
+    }
+}
--- Base URP with Firebase XRTK/Base URP with Firebase XRTK/Assets/Scripts/MilkSpawnSpot.cs
+++ Base URP with Firebase XRTK/Base URP with Firebase XRTK/Assets/Scripts/MilkSpawnSpot.cs
@@ -29,31 +29,6 @@ public class MilkSpawnSpot : MonoBehaviour
                     milkSpawnerScript.usedSpots.Remove(j);
                 }
             }
-            /*if (this.gameObject.tag == "spawnSpot0")
-            {
-                customerSpawnerScript.usedSpots.Remove("0");
-                orderSlipManagerScript.OrderSlipDisable(0);
-            }
-            if (this.gameObject.tag == "spawnSpot1")
-            {
-                customerSpawnerScript.usedSpots.Remove("1");
-                orderSlipManagerScript.OrderSlipDisable(1);
-            }
-            if (this.gameObject.tag == "spawnSpot2")
-            {
-                customerSpawnerScript.usedSpots.Remove("2");
-                orderSlipManagerScript.OrderSlipDisable(2);
-            }
-            if (this.gameObject.tag == "spawnSpot3")
-            {
-                customerSpawnerScript.usedSpots.Remove("3");
-                orderSlipManagerScript.OrderSlipDisable(3);
-            }
-            if (this.gameObject.tag == "spawnSpot4")
-            {
-                customerSpawnerScript.usedSpots.Remove("4");
-                orderSlipManagerScript.OrderSlipDisable(4);
-            }*/
         }
     }
 }
