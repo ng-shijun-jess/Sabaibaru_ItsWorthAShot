2065ddb551125d13247f21d27ce3b047dccf49a5
Ng shijun Jess <S10222791@connect.np.edu.sg>
2022-12-17 22:22:25 +0800

    linking things in game manager
    removing some text and commenting the score stuff in the game manager C# script

--- Base URP with Firebase XRTK/Base URP with Firebase XRTK/Assets/Scripts/GameManager.cs
+++ Base URP with Firebase XRTK/Base URP with Firebase XRTK/Assets/Scripts/GameManager.cs
@@ -14,9 +14,8 @@ public class GameManager : MonoBehaviour
 {
     public SimpleAuthManager auth;
 
-    public TextMeshProUGUI scoreText;
+    ///public TextMeshProUGUI scoreText;
     public TextMeshProUGUI totalMoneyEarnedText;
-    public TextMeshProUGUI gameOverText;
 
     public TextMeshProUGUI goTotalMoneyEarnedText;
     public TextMeshProUGUI goTotalCustomersServedText;
@@ -45,7 +44,8 @@ public class GameManager : MonoBehaviour
         {
             if (playerLives == 0)
             {
-                GameOver();
+                GameOver(); ///need to link this
+                Debug.Log("Game is over");
             }
         }
     }
@@ -57,8 +57,8 @@ public class GameManager : MonoBehaviour
 
     public void UpdateScore(int scoreToAdd)
     {
-        score += scoreToAdd;
-        scoreText.text = "Score: " + score;
+        ///score += scoreToAdd;
+        ///scoreText.text = "Score: " + score;
     }
     public void UpdateTotalMoneyEarned(int moneyToAdd)
     {
@@ -68,16 +68,21 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
+
         isGameActive = false;
         Time.timeScale = 0;
         if(!isPlayerStatUpdated)
         {
-            UpdatePlayerStat(this.score, xpPerGame, this.timePerGame, this.totalMoneyEarned, this.totalCustomersLeft, this.totalCustomersServed);
+            UpdatePlayerStat(this.score, xpPerGame, this.timePerGame, this.totalMoneyEarned, this.totalCustomersLeft, this.totalCustomersServed); ///need to link this
+            Debug.Log("playerStats is being updated");
         }
         isPlayerStatUpdated = true;
         ///gameOverText.gameObject.SetActive(true);
         ///restartButton.gameObject.SetActive(true);
         gameOverMenu.SetActive(true);
+
+        //updating the text in the Game Over Menu
+
         goTotalMoneyEarnedText.text = "Money Earned: $" + totalMoneyEarned;
         goTotalCustomersServedText.text = "Total Customer served: " + totalCustomersServed;
 
@@ -87,15 +92,15 @@ public class GameManager : MonoBehaviour
 
     public void UpdatePlayerStat(int score, int xp, int totalTimeSpent, int totalMoneyEarned, int totalCustomersLeft, int totalCustomersServed)
     {
-        firebaseMgr.UpdatePlayerStats(auth.GetCurrentUser().UserId, score, xp, totalTimeSpent, totalMoneyEarned, totalCustomersLeft, totalCustomersServed, auth.GetCurrentUserDisplayName());
+        firebaseMgr.UpdatePlayerStats(auth.GetCurrentUser().UserId, score, xp, totalTimeSpent, totalMoneyEarned, totalCustomersLeft, totalCustomersServed, auth.GetCurrentUserDisplayName()); ///need to link this
     }
     private void Start()
     {
         isGameActive = true;
         isPlayerStatUpdated = false;
-        score = 0;
-        totalMoneyEarned = 0;
-        UpdateScore(0);
+        ///score = 0;
+        ///totalMoneyEarned = 0;
+        ///UpdateScore(0);
 
        
 
